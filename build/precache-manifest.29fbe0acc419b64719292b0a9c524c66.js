25fce64218078ae3f683fda1ec0e1b09c4cead53
Neil Tate <57351607+Neil-Tate@users.noreply.github.com>
2020-05-01 19:20:02 +0200

    first commit

---
+++ build/precache-manifest.29fbe0acc419b64719292b0a9c524c66.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c862fec15f2bceb0f6a135325cebe88e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57f1181193f4f046a2b6",
+    "url": "/static/css/main.f993a34f.chunk.css"
+  },
+  {
+    "revision": "464bf3ea7681e9c4a95d",
+    "url": "/static/js/2.44028d75.chunk.js"
+  },
+  {
+    "revision": "ff5f972e9aecd52025a9769f32d18669",
+    "url": "/static/js/2.44028d75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57f1181193f4f046a2b6",
+    "url": "/static/js/main.2cb4b056.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
